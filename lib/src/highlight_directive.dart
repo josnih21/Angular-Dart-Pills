67bfa6675a4e86e159c680e5655c67ebef15e738
Jose Angel Nieda <josnih@gmail.com>
2021-01-07 15:52:11 +0000

    Bind highlighted color to a @Input property

--- lib/src/highlight_directive.dart
+++ lib/src/highlight_directive.dart
@@ -5,19 +5,16 @@ import 'package:angular/angular.dart';
 @Directive(selector: '[myHighlight]')
 class HighlightDirective {
   final Element _element;
-  HighlightDirective(this._element) {
-    _element.style.backgroundColor = 'yellow';
-  }
+  @Input('myHighlight')
+  String highlightColor;
+
+  HighlightDirective(this._element);
 
   @HostListener('mouseenter')
-  void onMouseEnter() {
-    _highlight(color: 'yellow');
-  }
+  void onMouseEnter() => _highlight(color: highlightColor ?? 'red');
 
   @HostListener('mouseleave')
-  void onMouseLeave() {
-    _highlight();
-  }
+  void onMouseLeave() => _highlight();
 
   void _highlight({String color}) {
     _element.style.backgroundColor = color;
